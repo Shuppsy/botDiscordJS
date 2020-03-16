8e3b4c5ba0a1be1122ac29e8386bb8dfdf2c4676
Shupps <bert.formation@gmail.com>
2020-03-16 05:16:28 +0100

    option client + optimisation

---
+++ main.js
@@ -0,0 +1,16 @@
+let { Client } = require('discord.js'),
+  userMsg = 'ping',
+  finalmsg = userMsg.toLowerCase(),
+  client = new Client({ disableEveryone: true });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === finalmsg) msg.channel.send('Pong!');
+  if (msg.content === 'everyone') msg.channel.send('@everyone, Salut à tous !', { disableEveryone: false });
+  if (msg.content === 'noteveryone') msg.channel.send('@everyone! (noteveryone) Salut à tous !');
+});
+
+client.login('Njg4OTMzOTgyMjI1NTYzNzE0.Xm7qBw.fcIezagUNbnAZZ4c8E6hwSBAyT8');
