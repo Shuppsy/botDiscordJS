93dddcae106448549aa5e3c182a94976f524a9a6
Shupps <bert.formation@gmail.com>
2020-03-27 06:37:29 +0100

    Revert "eval"
    This reverts commit 9bc9300153ceef4089b96a326b102661c2ef4d08.

--- commands/member.js
+++
@@ -1,6 +0,0 @@
-module.exports = (client, message) => {
-  message.channel.send('Les membres sont :');
-  let mb = client.users.cache.array(),
-    mbFinal = mb.slice(1);
-  mbFinal.forEach(user => message.channel.send(user.username));
-};
--- events/guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-module.exports = (client, member) => {
-  member.send('Bienvenue à toi !');
-  let channel = client.channels.cache.find(ch => ch.name === "logs");
-  channel.send(`${member} a rejoint le server !`);
-};
